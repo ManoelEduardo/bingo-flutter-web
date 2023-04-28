989526da1eb9e0b1e0976a40ec5b90c6adb797b0
Manoel Eduardo <manoeleduardo.sistemas@gmail.com>
2023-04-28 00:12:25 -0300

    Adjusting

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class Homecontroller extends ChangeNotifier {
+class Homecontroller {
   List<int> drawnNumbers = [];
   ValueNotifier<List<int>> letterBNumbers = ValueNotifier([]);
   ValueNotifier<List<int>> letterINumbers = ValueNotifier([]);
